3bc4ed87497697f7cdc887e5a006badbfc2ef0a4
ahrohn92 <ahrohn92@gmail.com>
2020-12-10 21:35:46 +0100

    added calculate and sum endpoints and tests

--- src/main/java/com/example/demo/EndpointsController.java
+++ src/main/java/com/example/demo/EndpointsController.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.Map;
 
 @RestController
 public class EndpointsController {
@@ -9,5 +11,45 @@ public class EndpointsController {
     @GetMapping("/math/pi")
     public String pi() {
         return "3.141592653589793";
+    }
+
+    @GetMapping("/math/calculate")
+    public String calculate(
+            @RequestParam(value = "operation", defaultValue = "add") String operation,
+            @RequestParam("x") String x,
+            @RequestParam("y") String y) {
+        if (operation.equals("add")) {
+            int sum = Integer.parseInt(x) + Integer.parseInt(y);
+            return x+" + "+y+" = "+sum;
+        }
+        if (operation.equals("multiply")) {
+            int product = Integer.parseInt(x) * Integer.parseInt(y);
+            return x+" * "+y+" = "+product;
+        }
+        if (operation.equals("subtract")) {
+            int difference = Integer.parseInt(x) - Integer.parseInt(y);
+            return x+" - "+y+" = "+difference;
+        }
+        if (operation.equals("divide")) {
+            int quotient = Integer.parseInt(x) / Integer.parseInt(y);
+            return x+" / "+y+" = "+quotient;
+        }
+        return "";
+    }
+
+    @PostMapping("/math/sum")
+    public String sum(@RequestParam int[] n) {
+        int sum = 0;
+        StringBuilder output = new StringBuilder("");
+        for (int i = 0; i < n.length; i++) {
+            if (i == 0) {
+                output.append(n[i]);
+            } else {
+                output.append(" + "+n[i]);
+            }
+            sum += n[i];
+        }
+        output.append(" = "+sum);
+        return output.toString();
     }
 }
--- src/test/java/com/example/demo/EndpointsControllerTest.java
+++ src/test/java/com/example/demo/EndpointsControllerTest.java
@@ -23,6 +23,53 @@ public class EndpointsControllerTest {
                 .andExpect(content().string("3.141592653589793"));
     }
 
+    @Test
+    public void testCalculateAdd() throws Exception {
+        RequestBuilder request = MockMvcRequestBuilders.get("/math/calculate?operation=add&x=4&y=6");
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string("4 + 6 = 10"));
+    }
+
+    @Test
+    public void testCalculateMultiply() throws Exception {
+        RequestBuilder request = MockMvcRequestBuilders.get("/math/calculate?operation=multiply&x=4&y=6");
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string("4 * 6 = 24"));
+    }
+
+    @Test
+    public void testCalculateSubtract() throws Exception {
+        RequestBuilder request = MockMvcRequestBuilders.get("/math/calculate?operation=subtract&x=4&y=6");
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string("4 - 6 = -2"));
+    }
+
+    @Test
+    public void testCalculateDivide() throws Exception {
+        RequestBuilder request = MockMvcRequestBuilders.get("/math/calculate?operation=divide&x=30&y=5");
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string("30 / 5 = 6"));
+    }
+
+    @Test
+    public void testCalculateDefaultOperation() throws Exception {
+        RequestBuilder request = MockMvcRequestBuilders.get("/math/calculate?x=30&y=5");
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string("30 + 5 = 35"));
+    }
+
+    @Test
+    public void testSum() throws Exception {
+        RequestBuilder request = MockMvcRequestBuilders.post("/math/sum?n=4&n=5&n=6");
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string("4 + 5 + 6 = 15"));
+    }
 }
 
 
