78fa88d85c2a50c39b4a8f5be689c93ef4d5208d
ahrohn92 <ahrohn92@gmail.com>
2020-12-14 21:54:27 +0100

    added test for the GET /lessons endpoint

---
+++ src/test/java/com/example/demo/LessonControllerTest.java
@@ -0,0 +1,50 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.hamcrest.core.IsInstanceOf.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.web.servlet.function.RequestPredicates.contentType;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class LessonControllerTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @Autowired
+    LessonRepository repository;
+
+    @Test
+    @Transactional
+    @Rollback
+    public void testReadLesson() throws Exception {
+        MockHttpServletRequestBuilder request1 = post("/lessons")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{    \"title\": \"SQL\",\n" +
+                        "    \"deliveredOn\": \"2017-04-06\"}");
+        this.mvc.perform(request1);
+
+        MockHttpServletRequestBuilder request2 = get("/lessons")
+                .accept(MediaType.APPLICATION_JSON);
+
+        this.mvc.perform(request2)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("[0].id", is(1)));
+
+    }
+}
